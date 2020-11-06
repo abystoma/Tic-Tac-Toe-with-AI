518be1bcfc4ab41b114315b814a0defe42af2e9d
b1rkenspanner <zayed.humayun@g.bracu.ac.bd>
2020-11-06 19:58:10 +0600

    Refactored the code

--- Watch 'em fight.py
+++ Watch 'em fight.py
@@ -1,7 +1,56 @@
 import random
 
 
-class TicTacToe:
+class Player:
+    def __init__(self, mark, opp_mark):
+        self.mark = mark
+        self.opp_mark = opp_mark
+
+
+class User(Player):
+    def move(self, field):
+        coord = self.coordinates(field)
+        field[coord] = self.mark
+        return field
+
+    def coordinates(self, field):
+        try:
+            x, y = [int(i) for i in input("Enter the coordinates:").split(" ")]
+            index = Game.coordinate_conversion[x, y]
+        except:
+            print("You should enter numbers!")
+            return self.coordinates(field)
+
+        if x not in range(1, 4) or y not in range(1, 4):
+            print("Coordinates should be from 1 to 3!")
+            return self.coordinates(field)
+
+        if field[index] in Game.mark:
+            print("This cell is occupied! Choose another one!")
+            return self.coordinates(field)
+
+        index = Game.coordinate_conversion[x, y]
+        return index
+
+
+class Robot(Player):
+    def move(self, field):
+        print(f'Making move level "{self.__class__.__name__.lower()}"')
+        coords_1 = self.coordinates(field)
+        field[coords_1] = self.mark
+        return field
+
+
+class Easy(Robot):
+    def coordinates(self, field):
+        coords = Game.coordinate_conversion[random.randint(
+            1, 3), random.randint(1, 3)]
+        if field[coords] != " ":
+            return self.coordinates(field)
+        return coords
+
+
+class Game:
 
     board = {1: " ", 2: " ", 3: " ",
              4: " ", 5: " ", 6: " ",
@@ -11,181 +60,69 @@ class TicTacToe:
                              (1, 2): 4, (2, 2): 5, (3, 2): 6,
                              (1, 1): 7, (2, 1): 8, (3, 1): 9}
 
+    command = {'user': User, 'easy': Easy, 'start': None, 'exit': exit}
+    mark = ['X', 'O']
+
     def __init__(self):
-        pass
+        self.field = Game.board.copy()
+        self.start()
+        print(self)
+        self.process()
 
-    def menu(self):
-        while True:
-            var = input("Input command: ")
+    def __str__(self):
+        result = f"---------\n" \
+                 f"| {self.field[1]} {self.field[2]} {self.field[3]} |\n" \
+                 f"| {self.field[4]} {self.field[5]} {self.field[6]} |\n" \
+                 f"| {self.field[7]} {self.field[8]} {self.field[9]} |\n" \
+                 f"---------"
+        return result
 
-            if var == "exit":
-                exit()
-            if len(var.split()) != 3:
-                print("Bad parameters!")
-                continue
-
-            else:
-                if var.split()[2] == "easy" and var.split()[1] == "easy":
-                    self.print_board()
-                    while True:
-                        self.moves_1()
-                elif var.split()[1] == "user" and var.split()[2] == "easy" or var.split()[2] == "user" and var.split()[1] == "easy":
-                    self.print_board()
-
-                    while True:
-                        self.coordinates()
-                elif var.split()[1] == "user" and var.split()[2] == "user":
-                    self.print_board()
-
-                    while True:
-                        self.moves_2()
-
-    def check(self):
-        try:
-            x, y = [int(i) for i in input("Enter the coordinates:").split(" ")]
-
-            # if x not in range(1, 4) or y not in range(1, 4):
-            #   print("Coordinates should be from 1 to 3!")
-            index = TicTacToe.coordinate_conversion[x, y]
-            # print(index)
-        except:
-            print("You should enter numbers!")
-
-        if x not in range(1, 4) or y not in range(1, 4):
-            print("Coordinates should be from 1 to 3!")
-            return None
-
-        if TicTacToe.board[index] != " ":
-            print("This cell is occupied! Choose another one!")
-            return None
-        return TicTacToe.coordinate_conversion[x, y]
-
-    def moves_2(self):
-        while True:
-            self.move_user_1(self.check())
-            if self.board_state():
-                print(self.board_state())
-                self.menu()
-            self.move_user_2(self.check())
-            if self.board_state():
-                print(self.board_state())
-                self.menu()
-
-    def moves_1(self):
-        self.move_AI_1()
-        if self.board_state():
-            print(self.board_state())
-            self.menu()
-        self.move_AI_2()
-        if self.board_state():
-            print(self.board_state())
-            self.menu()
-
-    def print_board(self):
-        print("---------")
-        print('| ' + TicTacToe.board[1] + ' ' +
-              TicTacToe.board[2] + ' ' + TicTacToe.board[3] + ' |')
-        print('| ' + TicTacToe.board[4] + ' ' +
-              TicTacToe.board[5] + ' ' + TicTacToe.board[6] + ' |')
-        print('| ' + TicTacToe.board[7] + ' ' +
-              TicTacToe.board[8] + ' ' + TicTacToe.board[9] + ' |')
-        print("---------")
-
-    def coordinates(self):
-
-        try:
-            x, y = [int(i) for i in input("Enter the coordinates:").split(" ")]
-
-            # if x not in range(1, 4) or y not in range(1, 4):
-            #   print("Coordinates should be from 1 to 3!")
-            index = TicTacToe.coordinate_conversion[x, y]
-            # print(index)
-        except:
-            print("You should enter numbers!")
-
-        if x not in range(1, 4) or y not in range(1, 4):
-            print("Coordinates should be from 1 to 3!")
-            return None
-
-        if TicTacToe.board[index] != " ":
-            print("This cell is occupied! Choose another one!")
-            return None
-        ind = TicTacToe.coordinate_conversion[x, y]
-        self.move_user_1(ind)
-        if self.board_state():
-            print(self.board_state())
-            self.menu()
-        self.move_AI_2()
-        if self.board_state():
-            print(self.board_state())
-            self.menu()
-
-    def move_user_1(self, index):
-        # print(index)
-        TicTacToe.board[index] = "X"
-        self.print_board()
-
-    def move_user_2(self, index):
-        # print(index)
-        TicTacToe.board[index] = "O"
-        self.print_board()
-
-    def move_AI_1(self):
-        print('Making move level "easy"')
-        while True:
-            index = TicTacToe.coordinate_conversion[random.randint(
-                1, 3), random.randint(1, 3)]
-            if TicTacToe.board[index] == " ":
-                TicTacToe.board[index] = "X"
-                break
-        self.print_board()
-
-    def move_AI_2(self):
-        print('Making move level "easy"')
-        while True:
-            index = TicTacToe.coordinate_conversion[random.randint(
-                1, 3), random.randint(1, 3)]
-            if TicTacToe.board[index] == " ":
-                TicTacToe.board[index] = "O"
-                break
-        self.print_board()
-
-    def board_wins(self, board, symbol):
-        # 3 horizontal checks for wins
-        if board[1] == board[2] == board[3] == symbol:
-            return True
-        elif board[4] == board[5] == board[6] == symbol:
-            return True
-        elif board[7] == board[8] == board[9] == symbol:
-            return True
-
-        # 3 vertical checks for wins
-        elif board[1] == board[4] == board[7] == symbol:
-            return True
-        elif board[2] == board[5] == board[8] == symbol:
-            return True
-        elif board[3] == board[6] == board[9] == symbol:
-            return True
-
-        # 2 diagonal checks for wins
-        elif board[1] == board[5] == board[9] == symbol:
-            return True
-        elif board[3] == board[5] == board[7] == symbol:
-            return True
-
+    def start(self):
+        params = input("Input command: ").split()
+        if params[0] == 'exit' and len(params) == 1:
+            exit()
+        elif params[0] == 'start' and len(params) == 3:
+            if params[1] in Game.command and params[2] in Game.command:
+                self.first = Game.command[params[1]]('X', 'O')
+                self.second = Game.command[params[2]]('O', 'X')
         else:
-            return False
+            print("Bad parameters!")
+            self.start()
 
-    def board_state(self):
-        if self.board_wins(TicTacToe.board, "X"):
-            return "X wins"
-        elif self.board_wins(TicTacToe.board, "O"):
-            return "O wins"
+    def process(self):
+        while True:
+            for player in [self.first, self.second]:
+                self.field = player.move(self.field)
+                print(self)
+                if Game.checker(self.field):
+                    return None
 
-        elif list(TicTacToe.board.values()).count(" ") == 0:
-            return "Draw"
+    @staticmethod
+    def checker(field):
+        result = ''
+        for check in Game.mark:
+            if field[5] == check:
+                if (field[8] == field[2] == check or
+                        field[4] == field[6] == check or
+                        field[3] == field[7] == check or
+                        field[1] == field[9] == check):
+                    result += check
+            if field[7] == check:
+                if (field[8] == field[9] == check or
+                        field[1] == field[4] == check):
+                    result += check
+            if field[3] == check:
+                if (field[2] == field[1] == check or
+                        field[6] == field[9] == check):
+                    result += check
+
+        if not result and not list(field.values()).count(" "):
+            print("Draw\n")
+            result = "Draw"
+        elif result:
+            print(result, 'wins\n')
+        return result
 
 
-Game = TicTacToe()
 while True:
-    Game.menu()
+    session = Game()
