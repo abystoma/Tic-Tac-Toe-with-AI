75208eb3d177c6347c0b004067ebae530c42e7e9
xLolicon <rafnorest3@gmail.com>
2020-11-10 20:25:17 +0600

    added comments

--- ttt_AI.py
+++ ttt_AI.py
@@ -4,13 +4,15 @@ from abc import ABC, abstractmethod
 
 
 class Player:
+    #3.
     def __init__(self, mark, player_type):
         self.mark = mark
         self.opp_mark = "O" if mark == "X" else "X"
         self.player_type = player_type
 
-
+#3a
 class User(Player):
+    #4a
     def move(self, fields):
         coord = self.coordinates(fields)
         fields[coord] = self.mark
@@ -29,8 +31,9 @@ class User(Player):
             print("You should enter numbers!")
             return self.coordinates(fields)
 
-
+#abstract sublass of 3. and superclass of 3b
 class Robot(ABC, Player):
+    #4b
     def move(self, fields):
         print(f'Making move level "{self.player_type.lower()}"')
         coord = self.coordinates(fields)
@@ -40,15 +43,16 @@ class Robot(ABC, Player):
     def coordinates(self, fields):
         pass
 
-
+#3b if easy version
 class Easy(Robot):
+    #Here, AI just marks a random position
     def coordinates(self, fields):
         coord = random.randint(0, 8)
         if fields[coord] != " ":
             return self.coordinates(fields)
         return coord
 
-
+#3b if medium version
 class Medium(Robot):
     def coordinates(self, fields):
         coord = self.two_row_check(fields)
@@ -75,7 +79,8 @@ class Medium(Robot):
 
         main_diagonal = [fields[0], fields[4], fields[8]]
         side_diagonal = [fields[2], fields[4], fields[6]]
-
+        """Below checks various cases below where the player is one mark away from a win
+        Then Returns a coordinate based on that.""" 
         for mark in mark_list:
             for index, col in enumerate(cols):
                 if col.count(mark) == 2 and col.count(' '):
@@ -108,6 +113,7 @@ class Game:
         self.process()
 
     def __str__(self):
+    #displays the board/field
         result = f"---------\n" \
             f"| {' '.join(self.fields[0:3])} |\n" \
             f"| {' '.join(self.fields[3:6])} |\n" \
@@ -115,13 +121,16 @@ class Game:
             f"---------"
         return result
 
+    #2. This method calls different other methods based on the command
     def start(self):
         params = input("Input command: ").split()
         if params[0] == 'exit' and len(params) == 1:
             exit()
         elif params[0] == 'start' and len(params) == 3:
+        #this block starts the game
             if params[1] in Game.command and params[2] in Game.command:
                 self.players = [
+                #a list whose elements calls the 3. constructor(s) from different subclasses 3a, 3b
                     Game.command[params[1]]('X', params[1]),
                     Game.command[params[2]]('O', params[2])
                 ]
@@ -133,6 +142,8 @@ class Game:
             self.start()
 
     def process(self):
+        #4. This loop controls the movement by calling 4a/4b method depending on the current player
+        #And it calls 4c after each move
         while True:
             for player in self.players:
                 player.move(self.fields)
@@ -141,9 +152,12 @@ class Game:
                     return None
 
     def checker(self):
+    #4c. as the name suggests this checks whether there is a win or a draw or not
         fields = self.fields
         result = ''
         for check in Game.mark:
+        #this loop checks the winning conditions for each marks
+        #It works by centreing 1 game mark at a time
             if fields[4] == check:
                 if (fields[7] == fields[1] == check or
                         fields[3] == fields[5] == check or
@@ -159,6 +173,7 @@ class Game:
                         fields[5] == fields[8] == check):
                     result += check
 
+    #If there is no win / draw '' is returned and the loop at 4 continues
         if not result and not fields.count(" "):
             print("Draw\n")
             result = "Draw"
@@ -166,6 +181,6 @@ class Game:
             print(result, 'wins\n')
         return result
 
-
+#1. This starts the program, calling the Game constructor and then the 2nd method
 while True:
     session = Game()
